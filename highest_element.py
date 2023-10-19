257028fb74b0fd40efb24251c4334ec0422ccb21
nayab-nisar <134673262+nayab-nisar@users.noreply.github.com>
2023-10-19 16:23:44 +0500

    Create highest_element.py

---
+++ highest_element.py
@@ -0,0 +1,8 @@
+# Sample list
+my_list = [10, 5, 20, 8, 15]
+
+# Find the largest element in the list
+largest_element = max(my_list)
+
+# Print the result
+print("The largest element in the list is:", largest_element)
